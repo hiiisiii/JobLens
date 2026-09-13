6afb4b53249ed0204417cd14168fd71b54548857
hiiisiii <8243.hk@gmail.com>
2026-09-14 04:36:04 +0900

    feat: export conversational tool service

--- src/index.ts
+++ src/index.ts
@@ -32,6 +32,7 @@ export * from "./application/preparation-service.js";
 export * from "./application/review-service.js";
 export * from "./application/outcome-service.js";
 export * from "./tooling/tool-contract.js";
+export * from "./tooling/joblens-tool-service.js";
 export * from "./cli/index.js";
 export * from "./cli/discovery-options.js";
 export * from "./cli/outcome-command.js";
